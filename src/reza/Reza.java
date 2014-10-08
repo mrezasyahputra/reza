982b67b2df07180e21c9a18b4bcfe1eaebaef87f
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:24:44 +0700

    commit

---
+++ src/reza/Reza.java
@@ -0,0 +1,12 @@
+
+package reza;
+
+public class Reza {
+
+    public static void main(String[] args) {
+String nama = "M Reza Syahputra";
+        String kelas = "SI 13 E";
+        String npm = "13311380";
+        System.out.println("Nama :"+nama +"\nKelas:"+kelas +"\nNPM  :"+npm);
+    }
+}
